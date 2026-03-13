ff462e64d206d7a19980173d231e3598391f7ed6
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-03-13 23:14:26 +0500

    "Setup prisma database"

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import 'dotenv/config'
 import app from './app.js'
+import { prisma } from './config/prisma.js'
 
 const PORT = process.env.PORT || 5000
+
+async function testDB () {
+  const users = await prisma.user.findMany()
+  console.log(users)
+}
+
+testDB()
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
