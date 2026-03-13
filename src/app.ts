eb9c7881301bcb56f747ae94f95a61af6f421021
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-03-13 22:29:08 +0500

    "Applied Rate limiting"

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
-
+import rateLimit from 'express-rate-limit'
 
 const app = express()
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+})
 
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
+app.use(limiter)
 
 app.get('/health', (req: Request, res: Response) => {
   res.json({
