9ec80792b11cf17164fdd31362d7aa47e5b24d49
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-03-15 17:58:49 +0500

    "Working on auth middleware"

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import 'dotenv/config'
+
+export const authenticate = (
+   req: Request,
+   res: Response,
+   next: NextFunction
+) => {
+   const authHeader = req.headers.authorization;
+
+   if(!authHeader){
+      return res.status(401).json({
+         message: "Token Missing"
+      })
+   }
+   
+   const token = authHeader.split(" ")[1]
+
+   try{
+      const decoded = jwt.verify(
+         token,
+         process.env.JWT_SECRET as string
+      )
+
+      req.user = decoded
+      
+      next()
+   }catch{
+      res.status(401).json({
+         message: "Invalid Token"
+      })
+   }
+}
