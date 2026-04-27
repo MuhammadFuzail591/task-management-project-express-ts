60872d5e7c5ce9f60c863d58535179bd44518f9f
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-04-27 22:08:33 +0500

    Adding API documentation

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,29 @@ import helmet from 'helmet'
 import rateLimit from 'express-rate-limit'
 import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/task.routes.js'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
 const app = express()
+
+const options = {
+  definition:{
+    openapi:'3.0.0',
+    info:{
+      title:"Task Management Project",
+      version:"1.0.0"
+    }
+  },
+  apis:['./routes/*.ts']
+}
+
+const swaggerSpec = swaggerJsdoc(options)
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100
 })
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
